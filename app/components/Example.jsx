7dffa965ec8c77f4f704bfcf74c700426c92f73f
admin <dimoon.lda@gmail.com>
2016-12-26 23:21:54 +0200

    added components and links to nav

---
+++ app/components/Example.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Example = React.createClass({
+    render: function () {
+        return (
+            <h1>Example component!</h1>
+        )
+    }
+});
+
+module.exports = Example;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Nav = React.createClass({
    render: function () {
        return (
-           <h1>Navigation</h1>
+           <div>
+               <Link to="/">Get Weather</Link>
+               <Link to="/about">About</Link>
+               <Link to="/example">Example</Link>
+           </div>
        )
    }
 });
