7eb705d0d0abb0fc5456df60e83bc81f5bc09c93
lutay.d <dmytro.lutai@roots.com.ua>
2016-12-28 18:36:34 +0200

    Changed Example component

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Example = () => {
     return (
-        <h1>Example component!</h1>
+        <div>
+            <h3>Examples.</h3>
+            <p>Welcome to examples page!</p>
+        </div>
     )
 };
 
